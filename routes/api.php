f738010af0957addb48f89ee934040efcf056cf9
Akhil U S <devakhilus@gmail.com>
2025-06-17 14:32:11 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/api/products', [HomeController::class, 'apiProducts']);
+
 
